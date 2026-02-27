e70fedc30d27889d591eb36d6f7690cdbd7ebe44
Kingusama29 <usamagaming123@gmail.com>
2026-02-27 08:28:32 +0700

    update output css and create page

--- app/Views/proyek/create.php
+++ app/Views/proyek/create.php
@@ -83,7 +83,7 @@
                     </button>
 
                     <!-- Daftar Dokumen -->
-                    <div class="overflow-hidden rounded-xl border border-gray-200">
+                    <div class="overflow-hidden border border-gray-200">
                         <div class="bg-gray-100 px-4 py-2 text-sm font-semibold text-text-primary text-center">
                             Daftar Dokumen
                         </div>
