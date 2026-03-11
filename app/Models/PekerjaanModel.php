ae362818ed2248b8711915ce47b528cc577e74fb
Kingusama29 <usamagaming123@gmail.com>
2026-03-11 11:05:34 +0700

    update ahscontroller, pekerjacontroller, env, dan database.php

--- app/Models/PekerjaanModel.php
+++ app/Models/PekerjaanModel.php
@@ -6,9 +6,10 @@ use CodeIgniter\Model;
 
 class PekerjaanModel extends Model
 {
+    protected $DBGroup          = 'estimator';
     protected $table            = 'pekerjaan_utama';
     protected $primaryKey       = 'id_pekerjaan';
-    protected $useAutoIncrement = true;
+    protected $useAutoIncrement = false;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
@@ -20,20 +21,17 @@ class PekerjaanModel extends Model
     protected array $casts = [];
     protected array $castHandlers = [];
 
-    // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
-    // Validation
     protected $validationRules      = [];
     protected $validationMessages   = [];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
-    // Callbacks
     protected $allowCallbacks = true;
     protected $beforeInsert   = [];
     protected $afterInsert    = [];
