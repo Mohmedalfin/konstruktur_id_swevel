edb1c7f450ec24c7111fb01597846ab7ac254deb
Mohmedalfin <mohmedalfin170505@gmail.com>
2026-03-11 10:05:43 +0700

    Fix syntax error returning blank data

--- public/ajax/ajax_rab.js
+++ public/ajax/ajax_rab.js
@@ -162,6 +162,9 @@
                              fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"/>
                         </svg>
+                    </td>
+                </tr>`;
+
             if (cat.items.length === 0) {
                 html += `
                     <tr class="subrow-${cat.id} ${subClass} bg-table-row border-b border-table-border">
