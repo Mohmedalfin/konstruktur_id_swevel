b56a4a410559e8628cb41b5cf0a2e6ecef0785d7
Kingusama29 <usamagaming123@gmail.com>
2026-06-16 22:07:08 +0700

    Fix bug display formatRupiah mengalikan desimal dari database di Purchase Request

--- public/assets/js/purchasing/purchase-request.js
+++ public/assets/js/purchasing/purchase-request.js
@@ -5,19 +5,19 @@ let currentPrId = null;
 
 // Helper to format Rupiah
 function formatRupiah(angka) {
-    let number_string = angka.toString().replace(/[^,\d]/g, ''),
-        split = number_string.split(','),
-        sisa = split[0].length % 3,
-        rupiah = split[0].substr(0, sisa),
-        ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+    let parsed = parseFloat(angka);
+    if (isNaN(parsed)) parsed = 0;
+    let number_string = Math.round(parsed).toString(),
+        sisa = number_string.length % 3,
+        rupiah = number_string.substr(0, sisa),
+        ribuan = number_string.substr(sisa).match(/\d{3}/gi);
 
     if (ribuan) {
         let separator = sisa ? '.' : '';
         rupiah += separator + ribuan.join('.');
     }
 
-    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-    return rupiah ? 'Rp ' + rupiah : '';
+    return 'Rp ' + rupiah;
 }
 
 function openDetailModal(id) {
